0055cfbb7f5752775f89ecbeade55246eeb4b602
Samuel Wilcox <samuelwilcox@outlook.com>
2026-04-03 23:43:16 +0100

    add initial snake project

---
+++ snake/snake.py
@@ -0,0 +1,115 @@
+import pygame
+import random
+import time
+from enum import Enum
+
+tile = 20
+plane = (40, 40)
+
+pygame.init()
+screen = pygame.display.set_mode((tile*plane[0], tile*plane[1]))
+
+running = True
+
+snake = [(0,0), (0,1), (0,2)]
+food = None
+
+def place_food():
+    global food
+
+    while True:
+        food = (random.randint(0, plane[0]), random.randint(0, plane[1]))
+
+        if food not in snake:
+            break
+
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+
+direction = Direction.DOWN
+
+def update_snake():
+    global snake
+    global running
+
+    head = snake[len(snake)-1]
+
+    match direction:
+        case Direction.UP:
+            head = (head[0], head[1]-1)
+        case Direction.DOWN:
+            head = (head[0], head[1]+1)
+        case Direction.LEFT:
+            head = (head[0]-1, head[1])
+        case Direction.RIGHT:
+            head = (head[0]+1, head[1])
+
+    if head in snake:
+        running = False
+
+    if food == head:
+        place_food()
+    else:
+        snake = snake[1:]
+
+    snake.append(head)
+
+def handle_inputs():
+    global running
+    global direction
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                running = False
+                continue
+            
+        if event.type == pygame.KEYDOWN:
+            match event.key:
+                case pygame.K_UP:
+                    direction = Direction.UP
+
+                case pygame.K_DOWN:
+                    direction = Direction.DOWN
+
+                case pygame.K_LEFT:
+                    direction = Direction.LEFT
+
+                case pygame.K_RIGHT:
+                    direction = Direction.RIGHT
+            
+place_food()
+
+while running:
+    screen.fill((0, 0, 0))
+
+    handle_inputs()
+    update_snake()
+
+    for part in snake:
+        x = tile * part[0]
+        y = tile * part[1]
+
+        pygame.draw.rect(
+            screen,
+            (0,255,0),
+            (x, y, tile, tile),
+        )
+
+    
+    pygame.draw.rect(
+        screen,
+        (0,255,0),
+        (food[0]*tile, food[1]*tile, tile, tile),
+    )
+
+    pygame.display.update()
+    time.sleep(0.2)
+
+# TODO
+#  - Remove time.sleep in favour of more responsive approach
+#  - Generally improve code/refactor
+#  - Add walls and/or teleporting
+#  - Add scoring
